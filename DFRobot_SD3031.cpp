00c439d54dafd5ec8f330194a7937dba28303c67
tangjie133 <1334875503@qq.com>
2026-07-02 15:05:21 +0800

    fix: 问题 (Fixes #44)

--- DFRobot_SD3031.cpp
+++ DFRobot_SD3031.cpp
@@ -357,7 +357,7 @@ void DFRobot_SD3031::enableFrequency(eFrequency_t fr)
   uint8_t reg2,reg3;
   readReg(SD3031_REG_CTR2, &reg2, 1);
   readReg(SD3031_REG_CTR3, &reg3, 1);
-  reg2 = reg2 | 0x21;
+  reg2 |= 0x90; // Set Bit 7 (Oscillator Enable) and Bit 4 (SQW Enable)
   reg3 = reg3 | fr;
   writeReg(SD3031_REG_CTR2, &reg2, 1);
   writeReg(SD3031_REG_CTR3, &reg3, 1);
